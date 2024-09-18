d6daa828e77afb6f7845a1c581cc893994cec661
ahoward <ara.t.howard@gmail.com>
2024-09-18 14:58:36 -0700

    wip @ 2024-09-18T14:58:35-07:00

--- api/ruby.js
+++ api/ruby.js
@@ -4,5 +4,7 @@ export default async function handler(request, response) {
     const execa = (await import('execa')).execaCommand;
     //const result = await execa('ruby -v');
     //return response.status(200).json(result);
-    return response.status(200).json(JSON.stringify(process.env));
+    process.env['PATH'] = process.env['PATH'] + ':/ruby33/bin';
+    const result = await execa('ls /');
+    return response.status(200).json({env:process.env, result:result});
 }
